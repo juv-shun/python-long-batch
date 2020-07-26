a5e40b21f377763d3d5e075e9504cd92e20a2106
Shun Fukusumi <shun.fukusumi@gmail.com>
2020-07-26 16:10:38 +0900

    myloggingを削除

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 
-import mylogging
+import logging
 
 from retry import retry
 
@@ -8,7 +8,7 @@ from runner import Runner, RunnerStatus
 
 INTERVAL: int = int(os.getenv('INTERVAL', 1))
 LOG_LEVEL: str = os.getenv('LOG_LEVEL', 'INFO')
-logger = mylogging.getLogger('long-batch', LOG_LEVEL)
+logger = logging.getLogger('long-batch', LOG_LEVEL)
 
 
 @retry(tries=3, delay=5, backoff=2, logger=logger)
